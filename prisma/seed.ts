fd4c5c8b651a7420faf85a61b99a9dbb60fd0be2
Lucas <Rodriguesdesouzal28@gmail.com>
2023-04-28 12:20:34 -0300

    feat: routes clients

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,54 +2,54 @@ import prisma from "../src/database/database";
 
 async function main() {
 
-    const users = await prisma.users.findMany({})
-    if (!users) {
-        await prisma.users.createMany({
-            data: [
-                {
-                    name: "lucas",
-                    email: "lucas@gmail.com",
-                    password: "123456"
-                },
-                {
-                    name: "mateus",
-                    email: "mateus@gmail.com",
-                    password: "123456"
-                },
-            ]
-        })
-    }
+    await prisma.clients.deleteMany({})
+    await prisma.address.deleteMany({})
+    await prisma.users.deleteMany({})
 
-    const address = await prisma.address.findFirst({})
-    if (!address) {
-       const a = await prisma.address.create({
-            data: {
-                cep: "81935072",
-                city: "curitiba",
-                number: "12",
-                state: "pr",
-                street: "rua naoki",
-            }
-        })
-    }
-    const address1 = await prisma.address.findFirst({})
-    if(!address1) return console.log("deu ruim ")
-   const a = await prisma.clients.createMany({
-        data: [
-            {
-                user: 1,
-                cpf: "11122233369",
-                phone: "41999995588",
-                birthday: `2002-04-25T16:30:00Z`,
-                name: "lucas",
-                excluded: false,
-                address: address1.id
-            }
-        ]
+    const user = await prisma.users.create({
+        data: {
+            name: "lucas",
+            email: "lucas@gmail.com",
+            password: "123456"
+        }
     })
-    
-};
 
+    await prisma.users.create({
+        data: {
+            name: "mateus",
+            email: "mateus@gmail.com",
+            password: "123456"
+        }
+    })
+
+
+    const address = await prisma.address.create({
+        data: {
+            cep: "81935072",
+            city: "curitiba",
+            number: "12",
+            state: "pr",
+            street: "rua naoki",
+        }
+
+    })
+
+    console.log(address)
+
+    const client = await prisma.clients.create({
+        data: {
+            user: user.id,
+            cpf: "11122233369",
+            phone: "41999995588",
+            birthday: `2002-04-25T16:30:00Z`,
+            name: "lucas",
+            excluded: false,
+            address: address.id
+        }
+    })
+    console.log(client)
+
+}
 main()
     .then(() => {
         console.log("Registro feito com sucesso")
