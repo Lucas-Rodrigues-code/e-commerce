0ac5bb4145cf4b7a6a19dc299b13d43b3037eead
lucas <Rodriguesdesouzal28@gmail.com>
2024-07-02 13:52:34 -0300

    feat: Update card titles to Portuguese in dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -32,9 +32,7 @@ export default async function DashboardPage({ params }: DashboardPageProps) {
         <div className="grid gap-4 grid-cols-3">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">
-                Total revenue
-              </CardTitle>
+              <CardTitle className="text-sm font-medium">Faturamento</CardTitle>
               <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
@@ -45,7 +43,7 @@ export default async function DashboardPage({ params }: DashboardPageProps) {
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CardTitle className="text-sm font-medium">Vendas</CardTitle>
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
@@ -66,7 +64,7 @@ export default async function DashboardPage({ params }: DashboardPageProps) {
         </div>
         <Card className="col-span-4">
           <CardHeader className="">
-            <CardTitle className="">Overview</CardTitle>
+            <CardTitle className="">Visão geral</CardTitle>
           </CardHeader>
           <CardContent className="pl-2">
             <Overview data={graphRevenue} />
